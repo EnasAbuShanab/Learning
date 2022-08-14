e0b08416918c526b165119b19c84fc1e2f8bb4ec
WEnas98 <104684526+EnasAbuShanab@users.noreply.github.com>
2022-08-14 17:28:22 +0000

    apply object data type

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
     hey=5
     alert(hey); 
 
-var name1 = ["Name", "Age", "contry"]
-document.getElementById("name").innerHTML=name1[0];
+//var name1 = ["Name", "Age", "contry"]
+
+ var name1 = {Name: "enas", Age:"25", contry:"jorden"}
+
+document.getElementById("name").innerHTML=name1.Name;
     
